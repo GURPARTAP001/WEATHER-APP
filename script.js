c620c2cb8efb56deb52abd45bbba51674c6ae501
Gurpartap <gurpartap6125@gmail.com>
2023-03-30 03:11:40 +0530

    added the card instead of table in the lower section

--- script.js
+++ script.js
@@ -35,40 +35,24 @@ submit_btn.addEventListener("click",(e)=>{
 })
 
 getweather("Delhi")
+
+
 // weather of other places
-fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Punjab', options)
-	.then(response => response.json())
-	.then((response) => 
-	{
-		// weather of punjab
-		console.log(response)
-		cloud_pct1.innerHTML =response.cloud_pct 
-		feels_like1.innerHTML= response.feels_like
-		temp1.innerHTML= response.temp
-		humidity3.innerHTML= response.humidity
-		min_temp1.innerHTML= response.min_temp
-		max_temp1.innerHTML= response.max_temp
-		wind_speed3.innerHTML= response.wind_speed
-		
-		
-	
-	})
-	.catch(err => console.error(err));
-
-
 	fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=America', options)
 	.then(response => response.json())
 	.then((response) => 
 	{
 		// weather of america
 		console.log(response)
-		cloud_pct2.innerHTML =response.cloud_pct 
-		feels_like2.innerHTML= response.feels_like
-		temp2.innerHTML= response.temp
-		humidity4.innerHTML= response.humidity
-		min_temp2.innerHTML= response.min_temp
-		max_temp2.innerHTML= response.max_temp
-		wind_speed4.innerHTML= response.wind_speed
+		cloud_pct1.innerHTML +=response.cloud_pct 
+		feels_like1.innerHTML+= response.feels_like
+		temp1.innerHTML+= response.temp
+		humidity3.innerHTML+= response.humidity
+		min_temp1.innerHTML+= response.min_temp
+		max_temp1.innerHTML+= response.max_temp
+		wind_speed3.innerHTML+= response.wind_speed
+		wind_degrees1.innerHTML+= response.wind_degrees
+
 	})
 	.catch(err => console.error(err));
 
@@ -78,12 +62,14 @@ fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Punjab', opt
 	{
 		// weather of RUSSIA
 		console.log(response)
-		cloud_pct3.innerHTML =response.cloud_pct 
-		feels_like3.innerHTML= response.feels_like
-		temp3.innerHTML= response.temp
-		humidity5.innerHTML= response.humidity
-		min_temp3.innerHTML= response.min_temp
-		max_temp3.innerHTML= response.max_temp
-		wind_speed5.innerHTML= response.wind_speed
+		cloud_pct2.innerHTML+=response.cloud_pct 
+		feels_like2.innerHTML+= response.feels_like
+		temp2.innerHTML+= response.temp
+		humidity4.innerHTML+= response.humidity
+		min_temp2.innerHTML+= response.min_temp
+		max_temp2.innerHTML+= response.max_temp
+		wind_speed4.innerHTML+= response.wind_speed
+		wind_degrees2.innerHTML+= response.wind_degrees
+		
 	})
 	.catch(err => console.error(err));
